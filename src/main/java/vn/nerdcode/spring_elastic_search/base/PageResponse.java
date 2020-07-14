ad840a14af0c8943cdb5e6b52eb3bb6ff55e2fac
minhlee <lengocminh@unimarmores.com>
2020-07-14 14:26:40 +0700

    remove uneccessary constructor

--- src/main/java/vn/nerdcode/spring_elastic_search/base/PageResponse.java
+++ src/main/java/vn/nerdcode/spring_elastic_search/base/PageResponse.java
@@ -2,16 +2,12 @@ package vn.nerdcode.spring_elastic_search.base;
 
 import java.io.Serializable;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
 public class PageResponse<T extends Serializable> {
 
